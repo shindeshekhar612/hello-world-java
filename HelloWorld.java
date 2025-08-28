ab49839e2788e69039948ed98be8c5b455821717
shindeshekhar612 <shindeshekhar612@gmail.com>
2025-08-28 08:58:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello Indiaaa!");    
+    System.out.println("Hello Indiaaaa!");    
     System.out.println("Hello Shiv!");
     System.out.println("Hello Shivay!");
     System.out.println("Hello Shailu!");
